fcf81a97f6aa4e14c4d0dfb6910fc5d854554ed2
cdcmaya <cdcmaya@gmail.com>
2019-11-30 21:08:53 +0700

    Initial...

--- repMessage.php
+++ repMessage.php
@@ -7,8 +7,7 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
-$channel_token = '2iEX/kCl/EvkVXIIVEEarpOFPfSjqIW7/D5SsNxlGAtMAhCPSeXIzlgCgKdBpNRFWyBdMWGfcsEQKwjZS9soJ3hmSDIllq9enlC6eZsm+91MJUXvEtrObJFmozB7m9Uksaqg10XmJzIBVjR4e2XbyAdB04t89/1O/w1cDnyilFU=';
-$channel_secret = '0fd52b7e11735fb95aa778ad1dcf7547';
+
 
 $content = file_get_contents('php://input');
 $events=json_decode($content,true);
